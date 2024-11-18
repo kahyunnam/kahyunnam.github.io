8b4f6a24e46825e11ae0b0f1f0012a88a8888777
kahyunnam <kahyunnam9@gmail.com>
2024-11-17 20:28:05 -0500

    Updates

---
+++ precache-manifest.13bf45c65a66e5fc394616ec4b8030c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7f636b0f1e67d9603fd32a1efdba59",
+    "url": "%7Busername%7D.github.io/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "%7Busername%7D.github.io/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "%7Busername%7D.github.io/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "%7Busername%7D.github.io/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "%7Busername%7D.github.io/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "0a267a1c4d571dcfbd25",
+    "url": "%7Busername%7D.github.io/static/js/runtime~main.36e29870.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "%7Busername%7D.github.io/static/media/logo.06e73328.svg"
+  }
+]);
